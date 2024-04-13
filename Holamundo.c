6c1324a0b8eac48985838ae219446006fd8d14f4
KevinCadenas12 <kcadenas@frba.utn.edu.ar>
2024-04-13 18:59:57 -0300

    primer commit

---
+++ Holamundo.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ 
+int main()
+{
+ printf("Hola mundo"); 
+}
+
+/*Estandares de C: 
+
+para c11: gcc Holamundo.c -std=gnu11 -o main
+para c18: gcc Holamundo.c -std=gnu18 -o main */
